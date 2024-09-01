97dafa97d3efa886240e93a868cf30f6b154631d
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2024-09-01 14:14:14 +0530

    Data_types.py

---
+++ Basic_Python/Data_types.py
@@ -0,0 +1,19 @@
+##### Integer #####
+a=10  #int
+print(a) #10
+print(type(a))  # <class 'int'>
+
+##### String #####
+c="20.05"  #string
+print(c)  #20.05
+print(type(c)) #<class 'str'>
+
+######## Conversion of an integer to Float ########
+b=float(a)
+print("int to float",b)  #int to float 10.0
+print(type(b))  #<class 'float'>
+
+######### Conversion of an string to Float #########
+d=float(c) #float
+print(type(d))  #<class 'float'>
+print("string to float",d)   #string to float 20.05
